ca3f91a9e8d1e29ea1fe531d40af4d0cea3f8d7a
kareem muhammad <kareem.gaad@gmail.com>
2021-09-20 18:31:55 +0200

    .

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,22 @@ import "./App.css";
 import Footer from "./components/Footer/Footer";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home/Home";
-
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Home />
-      <Footer />
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
+        <Footer />
+      </Router>
     </div>
   );
 }
